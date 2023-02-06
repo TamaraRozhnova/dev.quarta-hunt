ca784499c6fe8082260bef0b43138e8623bbcbe7
Chernikov Kirill <chernikov@addamant.ru>
2023-02-06 14:24:39 +0300

    92354 | Страница разделов каталога

---
+++ local/templates/quarta_new/components/bitrix/catalog/main/sections.php
@@ -0,0 +1,101 @@
+<?php
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+?>
+
+<div class="catalog">
+    <?
+    $APPLICATION->IncludeComponent("bitrix:news.list", "main_slider", [
+        "ACTIVE_DATE_FORMAT" => "d.m.Y",
+        "ADD_SECTIONS_CHAIN" => "N",
+        "AJAX_MODE" => "N",
+        "AJAX_OPTION_ADDITIONAL" => "",
+        "AJAX_OPTION_HISTORY" => "N",
+        "AJAX_OPTION_JUMP" => "N",
+        "AJAX_OPTION_STYLE" => "N",
+        "CACHE_FILTER" => "N",
+        "CACHE_GROUPS" => "N",
+        "CACHE_TIME" => "36000000",
+        "CACHE_TYPE" => "A",
+        "CHECK_DATES" => "Y",
+        "DETAIL_URL" => "",
+        "DISPLAY_BOTTOM_PAGER" => "N",
+        "DISPLAY_DATE" => "N",
+        "DISPLAY_NAME" => "N",
+        "DISPLAY_PICTURE" => "N",
+        "DISPLAY_PREVIEW_TEXT" => "N",
+        "DISPLAY_TOP_PAGER" => "N",
+        "FIELD_CODE" => [
+            0 => "ID",
+            1 => "CODE",
+            2 => "NAME",
+            3 => "PREVIEW_TEXT",
+            4 => "PREVIEW_PICTURE",
+            5 => "DETAIL_TEXT",
+            6 => "DETAIL_PICTURE",
+            7 => "IBLOCK_TYPE_ID",
+            8 => "IBLOCK_ID",
+            9 => "IBLOCK_CODE",
+            10 => "IBLOCK_NAME",
+        ],
+        "FILTER_NAME" => "",
+        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+        "IBLOCK_ID" => "14",
+        "IBLOCK_TYPE" => "sliders",
+        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+        "INCLUDE_SUBSECTIONS" => "N",
+        "MESSAGE_404" => "",
+        "NEWS_COUNT" => "20",
+        "PAGER_BASE_LINK_ENABLE" => "N",
+        "PAGER_DESC_NUMBERING" => "N",
+        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+        "PAGER_SHOW_ALL" => "N",
+        "PAGER_SHOW_ALWAYS" => "N",
+        "PAGER_TEMPLATE" => ".default",
+        "PAGER_TITLE" => "Новости",
+        "PARENT_SECTION" => "477",
+        "PARENT_SECTION_CODE" => "",
+        "PREVIEW_TRUNCATE_LEN" => "",
+        "PROPERTY_CODE" => [
+            0 => "DESCRIPTION",
+            1 => "LINK",
+        ],
+        "SET_BROWSER_TITLE" => "N",
+        "SET_LAST_MODIFIED" => "N",
+        "SET_META_DESCRIPTION" => "N",
+        "SET_META_KEYWORDS" => "N",
+        "SET_STATUS_404" => "N",
+        "SET_TITLE" => "N",
+        "SHOW_404" => "N",
+        "SORT_BY1" => "SORT",
+        "SORT_BY2" => "",
+        "SORT_ORDER1" => "ASC",
+        "SORT_ORDER2" => "",
+        "STRICT_SECTION_CHECK" => "N",
+        "COMPACT" => "Y"
+    ],
+        false
+    );
+
+    $APPLICATION->IncludeComponent(
+        "bitrix:catalog.section.list",
+        "main",
+        [
+            "IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
+            "IBLOCK_ID" => $arParams["IBLOCK_ID"],
+            "SECTION_ID" => false,
+            "CACHE_TYPE" => $arParams["CACHE_TYPE"],
+            "CACHE_TIME" => $arParams["CACHE_TIME"],
+            "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
+            "COUNT_ELEMENTS" => $arParams["SECTION_COUNT_ELEMENTS"],
+            "TOP_DEPTH" => $arParams["SECTION_TOP_DEPTH"],
+            "SECTION_URL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["section"],
+            "SHOW_PARENT_NAME" => $arParams["SECTIONS_SHOW_PARENT_NAME"],
+        ],
+        $component
+    );
+    ?>
+</div>
