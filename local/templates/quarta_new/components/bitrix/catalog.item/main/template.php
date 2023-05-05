42bc31ce2a74eee07ff2da5962f59c966e731e44
Egor Ovchinnikov <eovchinnikov@addamant.ru>
2023-05-05 14:15:36 +0300

    95705 | Добавил обработку отсутствия фото для catalog.item шаблона main

--- local/templates/quarta_new/components/bitrix/catalog.item/main/template.php
+++ local/templates/quarta_new/components/bitrix/catalog.item/main/template.php
@@ -70,7 +70,11 @@ $item = $arResult['ITEM'];
 
         <a href="<?= $item['DETAIL_PAGE_URL'] ?>">
             <figure>
-                <img src="<?= $item['PREVIEW_PICTURE']['SRC'] ?>" alt="<?= $item['NAME'] ?>"/>
+                <? if (!empty($item['PREVIEW_PICTURE']['SRC'])) {?>
+                    <img src="<?= $item['PREVIEW_PICTURE']['SRC'] ?>" alt="<?= $item['NAME'] ?>"/>
+                <?} else {?>
+
+                <? } ?>
             </figure>
         </a>
     </div>
