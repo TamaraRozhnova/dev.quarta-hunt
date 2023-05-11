dbc3681bdced5a4ae1c7d7fde7ba7b20cedc45cc
Alexander <shcheglakov@addamant.ru>
2023-05-11 13:09:48 +0600

    95327 | раздел Личный кабинет

--- local/templates/quarta_new/components/bitrix/main.profile/.default/script.js
+++ local/templates/quarta_new/components/bitrix/main.profile/.default/script.js
@@ -35,45 +35,62 @@ $( document ).ready(function() {
             });
         });
     }
-	$(document).on('blur', '.form-control[type="text"]', function () {
+    $(document).on('blur', '#phone', function () {
         let th = $(this);
         let thWrapper = th.closest('.input');
-        let phoneLength = $('#phone').val().replace(/[^0-9]/g,"");
-        let pattern = /^[a-z0-9_-]+@[a-z0-9-]+\.[a-z]{2,6}$/i;
-	    let mail = $('.form-control[name="EMAIL"]');
-
+        let phoneLength = th.val().replace(/[^0-9]/g,"");
+        
         removeAllHint(thWrapper);
 
-        if(mail.val() != ''){
-            
-            if(mail.val().search(pattern) == 0){
-                $('.btn-primary[name="save"]').prop("disabled", false);
-            }else{
-                createHint(thWrapper, 'Неверный формат e-mail');
-                $('.btn-primary[name="save"]').prop("disabled", true);
-            }
-        }else{
-            console.log(mail);
-            createHint(thWrapper, 'Поле e-mail не должно быть пустым!');
-            $('.btn-primary[name="save"]').prop("disabled", true);
-        }
-
-        th.each(function(){
-            if ($(this).val().length == 0 && $(this).prop('name') !='PERSONAL_PHONE' && $(this).prop('name') !='EMAIL') {
-                createHint(thWrapper, 'Поле обязательно к заполнению');
-                
-                $('.btn-primary[name="save"]').prop("disabled", true);
-            }else{
-                $('.btn-primary[name="save"]').prop("disabled", false);
-            }
-        });
-        
         if(phoneLength.length < 11){
             createHint(thWrapper, 'Телефон должен быть в указанном формате');
             $('.btn-primary[name="save"]').prop("disabled", true);
         }else{
             $('.btn-primary[name="save"]').prop("disabled", false);
         }
+    });
+	$(document).on('blur', '.form-control[name="EMAIL"]', function () {
+        let th = $(this);
+        let thWrapper = th.closest('.input');
+        let pattern = /^[a-z0-9_-]+@[a-z0-9-]+\.[a-z]{2,6}$/i;
+
+        if(th.val() != ''){
+            if(th.val().search(pattern) == 0){
+                $('.btn-primary[name="save"]').prop("disabled", false);
+            }else{
+                createHint(thWrapper, 'Неверный формат e-mail');
+                $('.btn-primary[name="save"]').prop("disabled", true);
+            }
+        }else{
+            createHint(thWrapper, 'Поле e-mail не должно быть пустым!');
+            $('.btn-primary[name="save"]').prop("disabled", true);
+        }
+    });
+    $(document).on('blur', '.form-control[name="NAME"]', function () {
+        let th = $(this);
+        let thWrapper = th.closest('.input');
+
+        th.each(function(){
+            if (th.val().length == 0) {
+                createHint(thWrapper, 'Поле обязательно к заполнению');
+                $('.btn-primary[name="save"]').prop("disabled", true);
+            }else{
+                $('.btn-primary[name="save"]').prop("disabled", false);
+            }
+        }); 
+    });
+    $(document).on('blur', '.form-control[name="LAST_NAME"]', function () {
+        let th = $(this);
+        let thWrapper = th.closest('.input');
+
+        th.each(function(){
+            if (th.val().length == 0) {
+                createHint(thWrapper, 'Поле обязательно к заполнению');
+                $('.btn-primary[name="save"]').prop("disabled", true);
+            }else{
+                $('.btn-primary[name="save"]').prop("disabled", false);
+            }
+        }); 
     });
 });
 
