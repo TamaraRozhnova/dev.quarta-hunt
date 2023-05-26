51e05873f2c62efcf280edd34517435791a8fed5
Egor Ovchinnikov <eovchinnikov@addamant.ru>
2023-05-26 14:29:19 +0300

    96102 | Добавил модальное окно для отображения фото в карточке товара

--- local/templates/quarta_new/components/bitrix/catalog/main/bitrix/catalog.element/main/template.php
+++ local/templates/quarta_new/components/bitrix/catalog/main/bitrix/catalog.element/main/template.php
@@ -245,3 +245,18 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
         'params' => $arParams
     ], ['SHOW_BORDER' => false]); ?>
 </div>
+
+<div id="detail-card-modal-photo" class="modal">
+    <div class="modal__close">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-x" viewBox="0 0 16 16">
+            <path
+                d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+        </svg>
+    </div>
+    <div class="modal-content">
+        <div class="modal-body">
+            <img class="detail-card-image" src="" alt="">
+        </div>
+    </div>
+</div>
