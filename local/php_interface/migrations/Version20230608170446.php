bde852177402426ac94d2dbb6c17219ea8188d2c
Egor Ovchinnikov <eovchinnikov@addamant.ru>
2023-06-08 17:05:03 +0300

    96742 | Миграция на форму Контакты

---
+++ local/php_interface/migrations/Version20230608170446.php
@@ -0,0 +1,228 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version20230608170446 extends Version
+{
+    protected $description = "96742 | Веб-форма контакты";
+
+    protected $moduleVersion = "4.2.4";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+        $formHelper = $helper->Form();
+        $formId = $formHelper->saveForm(array (
+  'NAME' => 'Контакты',
+  'SID' => 'SIMPLE_FORM_5',
+  'BUTTON' => 'Сохранить',
+  'C_SORT' => '300',
+  'FIRST_SITE_ID' => NULL,
+  'IMAGE_ID' => NULL,
+  'USE_CAPTCHA' => 'N',
+  'DESCRIPTION' => '',
+  'DESCRIPTION_TYPE' => 'text',
+  'FORM_TEMPLATE' => '',
+  'USE_DEFAULT_TEMPLATE' => 'Y',
+  'SHOW_TEMPLATE' => NULL,
+  'MAIL_EVENT_TYPE' => 'FORM_FILLING_SIMPLE_FORM_5',
+  'SHOW_RESULT_TEMPLATE' => NULL,
+  'PRINT_RESULT_TEMPLATE' => NULL,
+  'EDIT_RESULT_TEMPLATE' => NULL,
+  'FILTER_RESULT_TEMPLATE' => NULL,
+  'TABLE_RESULT_TEMPLATE' => NULL,
+  'USE_RESTRICTIONS' => 'N',
+  'RESTRICT_USER' => '0',
+  'RESTRICT_TIME' => '0',
+  'RESTRICT_STATUS' => '',
+  'STAT_EVENT1' => 'form',
+  'STAT_EVENT2' => '',
+  'STAT_EVENT3' => '',
+  'LID' => NULL,
+  'C_FIELDS' => '0',
+  'QUESTIONS' => '4',
+  'STATUSES' => '1',
+  'arSITE' => 
+  array (
+    0 => 's1',
+  ),
+  'arMENU' => 
+  array (
+    'ru' => '',
+    'en' => '',
+  ),
+  'arGROUP' => 
+  array (
+  ),
+  'arMAIL_TEMPLATE' => 
+  array (
+  ),
+));
+        $formHelper->saveStatuses($formId, array (
+  0 => 
+  array (
+    'CSS' => 'statusgreen',
+    'C_SORT' => '100',
+    'ACTIVE' => 'Y',
+    'TITLE' => 'DEFAULT',
+    'DESCRIPTION' => 'DEFAULT',
+    'DEFAULT_VALUE' => 'Y',
+    'HANDLER_OUT' => '',
+    'HANDLER_IN' => '',
+  ),
+));
+        $formHelper->saveFields($formId, array (
+  0 => 
+  array (
+    'ACTIVE' => 'Y',
+    'TITLE' => 'Фамилия Имя Отчество',
+    'TITLE_TYPE' => 'text',
+    'SID' => 'SIMPLE_QUESTION_140',
+    'C_SORT' => '100',
+    'ADDITIONAL' => 'N',
+    'REQUIRED' => 'N',
+    'IN_FILTER' => 'Y',
+    'IN_RESULTS_TABLE' => 'Y',
+    'IN_EXCEL_TABLE' => 'Y',
+    'FIELD_TYPE' => 'text',
+    'IMAGE_ID' => NULL,
+    'COMMENTS' => '',
+    'FILTER_TITLE' => 'Фамилия Имя Отчество',
+    'RESULTS_TABLE_TITLE' => 'Фамилия Имя Отчество',
+    'ANSWERS' => 
+    array (
+      0 => 
+      array (
+        'MESSAGE' => ' ',
+        'VALUE' => '',
+        'FIELD_TYPE' => 'text',
+        'FIELD_WIDTH' => '0',
+        'FIELD_HEIGHT' => '0',
+        'FIELD_PARAM' => '',
+        'C_SORT' => '0',
+        'ACTIVE' => 'Y',
+      ),
+    ),
+    'VALIDATORS' => 
+    array (
+    ),
+  ),
+  1 => 
+  array (
+    'ACTIVE' => 'Y',
+    'TITLE' => 'Email',
+    'TITLE_TYPE' => 'text',
+    'SID' => 'SIMPLE_QUESTION_776',
+    'C_SORT' => '200',
+    'ADDITIONAL' => 'N',
+    'REQUIRED' => 'N',
+    'IN_FILTER' => 'Y',
+    'IN_RESULTS_TABLE' => 'Y',
+    'IN_EXCEL_TABLE' => 'Y',
+    'FIELD_TYPE' => 'text',
+    'IMAGE_ID' => NULL,
+    'COMMENTS' => '',
+    'FILTER_TITLE' => 'Email',
+    'RESULTS_TABLE_TITLE' => 'Email',
+    'ANSWERS' => 
+    array (
+      0 => 
+      array (
+        'MESSAGE' => ' ',
+        'VALUE' => '',
+        'FIELD_TYPE' => 'text',
+        'FIELD_WIDTH' => '0',
+        'FIELD_HEIGHT' => '0',
+        'FIELD_PARAM' => '',
+        'C_SORT' => '0',
+        'ACTIVE' => 'Y',
+      ),
+    ),
+    'VALIDATORS' => 
+    array (
+    ),
+  ),
+  2 => 
+  array (
+    'ACTIVE' => 'Y',
+    'TITLE' => 'Телефон',
+    'TITLE_TYPE' => 'text',
+    'SID' => 'SIMPLE_QUESTION_675',
+    'C_SORT' => '300',
+    'ADDITIONAL' => 'N',
+    'REQUIRED' => 'N',
+    'IN_FILTER' => 'Y',
+    'IN_RESULTS_TABLE' => 'Y',
+    'IN_EXCEL_TABLE' => 'Y',
+    'FIELD_TYPE' => 'text',
+    'IMAGE_ID' => NULL,
+    'COMMENTS' => '',
+    'FILTER_TITLE' => 'Телефон',
+    'RESULTS_TABLE_TITLE' => 'Телефон',
+    'ANSWERS' => 
+    array (
+      0 => 
+      array (
+        'MESSAGE' => ' ',
+        'VALUE' => '',
+        'FIELD_TYPE' => 'text',
+        'FIELD_WIDTH' => '0',
+        'FIELD_HEIGHT' => '0',
+        'FIELD_PARAM' => '',
+        'C_SORT' => '0',
+        'ACTIVE' => 'Y',
+      ),
+    ),
+    'VALIDATORS' => 
+    array (
+    ),
+  ),
+  3 => 
+  array (
+    'ACTIVE' => 'Y',
+    'TITLE' => 'Вопрос',
+    'TITLE_TYPE' => 'text',
+    'SID' => 'SIMPLE_QUESTION_146',
+    'C_SORT' => '800',
+    'ADDITIONAL' => 'N',
+    'REQUIRED' => 'N',
+    'IN_FILTER' => 'Y',
+    'IN_RESULTS_TABLE' => 'Y',
+    'IN_EXCEL_TABLE' => 'Y',
+    'FIELD_TYPE' => 'text',
+    'IMAGE_ID' => NULL,
+    'COMMENTS' => '',
+    'FILTER_TITLE' => 'Вопрос',
+    'RESULTS_TABLE_TITLE' => 'Вопрос',
+    'ANSWERS' => 
+    array (
+      0 => 
+      array (
+        'MESSAGE' => ' ',
+        'VALUE' => '',
+        'FIELD_TYPE' => 'textarea',
+        'FIELD_WIDTH' => '0',
+        'FIELD_HEIGHT' => '0',
+        'FIELD_PARAM' => '',
+        'C_SORT' => '0',
+        'ACTIVE' => 'Y',
+      ),
+    ),
+    'VALIDATORS' => 
+    array (
+    ),
+  ),
+));
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
+
