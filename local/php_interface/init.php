464cda1c79de49b6519c1b15c763f1f64ba6bd5f
root <root@shop2.fvds.ru>
2023-08-11 14:15:00 +0300

    main | hotfix отправки почты при регистрации

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -111,8 +111,9 @@ class CUserEx{
                     "EVENT_NAME" => "NEW_USER_COUPON", 
                     "LID" => "s1", 
                     "C_FIELDS" => array( 
-                        "EMAIL" => "doubleboys@yandex.ru", 
-                        "USER_ID" => $arFields["USER_ID"] 
+                        "EMAIL" => $arFields["EMAIL"], 
+                        "USER_ID" => $arFields["USER_ID"],
+                        "PROMO_CODE" => $COUPON,
                     ), 
                 ));
 
