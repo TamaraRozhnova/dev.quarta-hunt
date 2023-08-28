1a5e923546859a24c2fac7c073a826d4f7c49098
Egor Ovchinnikov <eovchinnikov@addamant.ru>
2023-08-28 11:00:59 +0300

    99343 | Правка маски номера для регистрации и авторизации + поправил блоки, ранее выходили за границы

---
+++ local/templates/quarta_new/components/custom/main.register/custom/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    if ($('#phone').length != 0) {
+        $('#phone').inputmask({"mask": "+7 (999) 999-99-99"});
+        $('#phone').val('')
+    }
+
+    const phoneInput = document.querySelector('#phone');
+
+    phoneInput.addEventListener('paste', (event) => {
+        clipboardData = event.clipboardData || window.clipboardData;
+
+        pastedData = clipboardData.getData('Text');
+
+        if (pastedData.match(/\d+/g) != null) {
+            let modifyResult = pastedData.match(/\d+/g).join('');
+
+            if (modifyResult.length == 11) {
+                phoneInput.value = modifyResult.substring(1)
+            }
+
+        }
+
+    })
+
+    if ($('#promo').prop('checked') == true) {
+		$('input[name="UF_PROMO"]').val('true');
+	}
+
+	$('#promo').on('click', function(){
+		if ($(this).prop('checked') == true){
+			$('input[name="UF_PROMO"]').val('true');
+		}else{
+			$('input[name="UF_PROMO"]').val('false');
+		}
+	});
+
+})
+
