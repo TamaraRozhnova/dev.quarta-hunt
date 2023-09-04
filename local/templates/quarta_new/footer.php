4aa2055f75c7a79367ea6dde6f39b1e9d64b9741
Egor Ovchinnikov <eovchinnikov@addamant.ru>
2023-09-04 17:15:28 +0300

    98971 | Метрики

--- local/templates/quarta_new/footer.php
+++ local/templates/quarta_new/footer.php
@@ -248,5 +248,10 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
         });
     </script>
 
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NJBCN2ZG"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     </body>
 </html>
