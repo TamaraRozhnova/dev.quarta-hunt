0e7ed7b32f0dff89f01ff2103468f7696e59616e
aminakin <46899180+aminakin@users.noreply.github.com>
2023-11-10 18:50:56 +0200

    102602 | Общее / Хлебные крошки - нужно добавить сворачивание ХК в облако в мобильной версии

--- local/templates/quarta_new/components/bitrix/breadcrumb/.default/template.php
+++ local/templates/quarta_new/components/bitrix/breadcrumb/.default/template.php
@@ -22,9 +22,14 @@ if(!is_array($css) || !in_array("/bitrix/css/main/font-awesome.css", $css))
 	$strReturn .= '<link href="'.CUtil::GetAdditionalFileURL("/bitrix/css/main/font-awesome.css").'" type="text/css" rel="stylesheet" />'."\n";
 }
 
+$itemSize = count($arResult);
+
+if ($itemSize > 2) {
+	$strReturn .= '<div class="black-bg" style="display: none;"></div><div class="breadcrumb__popup"><div class="cross"></div>';
+}
+
 $strReturn .= '<div class="bx-breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">';
 
-$itemSize = count($arResult);
 for($index = 0; $index < $itemSize; $index++)
 {
 	$title = htmlspecialcharsex($arResult[$index]["TITLE"]);
@@ -51,6 +56,46 @@ for($index = 0; $index < $itemSize; $index++)
 	}
 }
 
+unset($index);
+
 $strReturn .= '<div style="clear:both"></div></div>';
+
+if ($itemSize > 2) {
+	$strReturn .= '</div><div class="bx-breadcrumb breadcrumb__fake">';
+	for($index = 0; $index < $itemSize; $index++) {
+
+		$title = htmlspecialcharsex($arResult[$index]["TITLE"]);
+		$arrow = ($index > 0? '<i class="fa fa-angle-right"></i>' : '');
+
+		if ($index > 0 && $index < $itemSize - 1) {
+			continue;
+		}
+
+		if($arResult[$index]["LINK"] <> "" && $index != $itemSize-1) {
+			$strReturn .= '
+				<div class="bx-breadcrumb-item">
+					'.$arrow.'
+					<a href="'.$arResult[$index]["LINK"].'" title="'.$title.'">
+						<span>'.$title.'</span>
+					</a>					
+				</div>';
+		}
+		else
+		{
+			$strReturn .= '
+				<div class="bx-breadcrumb-item">
+					'.$arrow.'
+					<span>'.$title.'</span>
+				</div>';
+		}
+
+		if ($index == 0) {
+			$strReturn .= '<div class="bx-breadcrumb-item breadcrumbs__dots"><span>...</span></div>';
+		}
+	}
+	$strReturn .= '<div style="clear:both"></div></div>';
+}
+
+
 
 return $strReturn;
