d4d5a7644b322778aa2272c8f956265d0e633493
eo-33 <eovchinnikov@addamant.ru>
2023-11-23 15:52:39 +0300

    102899 | Добавил генерацию реф.купонов

--- local/templates/quarta_new/components/bitrix/sale.personal.section/template/template.php
+++ local/templates/quarta_new/components/bitrix/sale.personal.section/template/template.php
@@ -235,6 +235,21 @@ if (empty($availablePages)){
 								</div>
 
 							</div>
+
+							<?$APPLICATION->IncludeComponent(
+								"logictim:bonus.history",
+								"cabinet",
+								Array(
+									"FIELDS" => array("ID","DATE","NAME","OPERATION_SUM","BALLANCE_BEFORE","BALLANCE_AFTER"),
+									"ORDER_LINK" => "N",
+									"OPERATIONS_WAIT" => "Y",
+									"ORDER_URL" => "/personal/order/",
+									"PAGE_NAVIG_LIST" => "30",
+									"PAGE_NAVIG_TEMP" => "arrows",
+									"SORT" => "DESC"
+								)
+							);?>
+
 						</div>
 					</div>
 
---
+++ local/templates/quarta_new/components/logictim/bonus.history/cabinet/lang/ru/template.php
@@ -0,0 +1,4 @@
+<?php
+$MESS["LOGICTIM_REFERALS_REF_COUPON"] = "Код реферального купона";
+$MESS["LOGICTIM_REFERALS_REF_COUPON_GENERATE"] = "Сгенерировать";
+$MESS["LOGICTIM_REFERALS_REF_COUPON_USED"] = "Кодом воспользовались #USED_COUNT# раз";
