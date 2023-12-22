73a474624fbeff2960ac67023b34fc3475948b65
Denisov-Sergey <sithis206@gmail.com>
2023-12-22 16:56:16 +0600

    103934 | Компонент для рассылки

---
+++ local/components/custom/sale.basket.basket.small.mail/class.php
@@ -0,0 +1,116 @@
+<?php
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+use Bitrix\Iblock\ElementTable;
+use Bitrix\Main\Loader;
+use	Bitrix\Sale;
+
+CBitrixComponent::includeComponentClass("bitrix:sale.basket.basket");
+
+class CBitrixBasketBasketSmallMailComponent extends CBitrixBasketComponent
+{
+	protected int $userId;
+
+    CONST LICENCE_CAT = 583;
+
+	public function onPrepareComponentParams($params)
+	{
+		$columnList = $params['COLUMNS_LIST'] ?? [];
+		if (empty($columnList) || !is_array($columnList))
+		{
+			$columnList = [
+				'PREVIEW_PICTURE',
+				'NAME',
+				'SUM',
+			];
+		}
+		$params = parent::onPrepareComponentParams($params);
+		$this->columns = $columnList;
+
+		$this->userId = (int)($params['USER_ID'] ?? 0);
+
+		$fuserId = (int)($params['FUSER_ID'] ?? 0);
+		if ($fuserId > 0)
+		{
+			$this->fUserId = $fuserId;
+		}
+		else
+		{
+			$this->fUserId = Sale\Fuser::getIdByUserId($this->userId);
+		}
+
+		$params['COMPATIBLE_MODE'] = 'Y';
+
+		if (!$this->getSiteId())
+		{
+			$siteId = (string)($params['LID'] ?? CSite::GetDefSite());
+			$this->setSiteId($siteId);
+		}
+
+		return $params;
+	}
+
+	protected function sortItemsByTabs(&$result)
+	{
+		$result['ITEMS'] = array(
+			'AnDelCanBuy' => array(),
+			'DelDelCanBuy' => array(),
+			'nAnCanBuy' => array(),
+			'ProdSubscribe' => array()
+		);
+
+        $result['IS_LICENCE_PRODUCT'] = false;
+
+		if (!empty($this->basketItems))
+		{
+			foreach ($this->basketItems as $item)
+			{
+
+                $arSections = $this->isLicenseProduct($item);
+                if (in_array(self::LICENCE_CAT, $arSections)){
+                    $result['IS_LICENCE_PRODUCT'] = true;
+                }
+
+				if ($item['CAN_BUY'] === 'Y' && $item['DELAY'] !== 'Y')
+				{
+					$result['ITEMS']['AnDelCanBuy'][] = $item;
+				}
+				elseif ($this->arParams['SHOW_DELAY'] === 'Y' && $item['CAN_BUY'] === 'Y' && $item['DELAY'] === 'Y')
+				{
+					$result['ITEMS']['DelDelCanBuy'][] = $item;
+				}
+				elseif ($this->arParams['SHOW_SUBSCRIBE'] === 'Y' && $item['CAN_BUY'] !== 'Y' && $item['SUBSCRIBE'] === 'Y')
+				{
+					$result['ITEMS']['ProdSubscribe'][] = $item;
+				}
+				elseif ($this->arParams['SHOW_NOTAVAIL'] === 'Y')
+				{
+					$result['ITEMS']['nAnCanBuy'][] = $item;
+				}
+			}
+		}
+
+		$result['ShowReady'] = !empty($result['ITEMS']['AnDelCanBuy']) ? 'Y' : 'N';
+		$result['ShowDelay'] = !empty($result['ITEMS']['DelDelCanBuy']) ? 'Y' : 'N';
+		$result['ShowNotAvail'] = !empty($result['ITEMS']['nAnCanBuy']) ? 'Y' : 'N';
+		$result['ShowSubscribe'] = !empty($result['ITEMS']['ProdSubscribe']) ? 'Y' : 'N';
+	}
+
+    protected function isLicenseProduct($item)
+    {
+        Loader::includeModule('iblock');
+
+        $dbGroups = CIBlockElement::GetElementGroups($item['PRODUCT_ID']);
+        while ($arGroup = $dbGroups->Fetch()) {
+            $subSections[] = $arGroup['ID'];
+        }
+
+        foreach ($subSections as $subSectionItem) {
+            foreach (CIBlockSection::GetNavChain(false, $subSectionItem, ['ID'], true) as $sectionItem){
+                $arSections[] = $sectionItem['ID'];
+            }
+        }
+
+        return $arSections;
+    }
+}
