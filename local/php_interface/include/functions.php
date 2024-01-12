d0a7e0634ab9f6035dc77ee15c2422ae26a1c824
root <root@shop2.fvds.ru>
2024-01-12 13:30:20 +0300

    104323 | удаление лишних функций

--- local/php_interface/include/functions.php
+++ local/php_interface/include/functions.php
@@ -1,16 +1,2 @@
 <?php
 
-function getRootProductSection($iblockId, $sectionId) {
-    $arSections = [];
-    while($sectionId) {
-        if ($arSection = \Bitrix\Iblock\SectionTable::getList([
-            'filter' => ['IBLOCK_ID' => $iblockId, 'ID' => $sectionId],
-            'select' => ['ID', 'IBLOCK_SECTION_ID', 'CODE']
-        ])->fetch()) {
-            $arSections[] = $arSection;
-        }
-        $sectionId = $arSection['IBLOCK_SECTION_ID'];
-    }
-    $arSections = array_reverse($arSections);
-    return $arSections;
-}
