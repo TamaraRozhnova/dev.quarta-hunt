3d3134893af2ef18cb22e40ace57f4c18609f901
eo-33 <eovchinnikov@addamant.ru>
2024-05-03 13:02:19 +0300

    109903 | Сайт / СЕО / Проверка полей ввода регистрации и авторизации

--- local/templates/quarta_new/components/custom/main.register/custom/script.js
+++ local/templates/quarta_new/components/custom/main.register/custom/script.js
@@ -1,4 +1,68 @@
 $(document).ready(function() {
+
+    /** 
+     * Validation fields for registration 
+    */
+
+    const phoneReg = new Input({
+        wrapperSelector: `.re-phone-reg`,
+        required: true,
+        validMask: /^\+7\s\([0-9]{3}\)\s[0-9]{3}\-[0-9]{2}\-[0-9]{2}$/,
+        mask: '+7 (###) ###-##-##',
+        errorMessage: 'Телефон должен быть в указанном формате'
+    });
+
+    const nameReg = new Input({
+        wrapperSelector: `.re-name-reg`,
+        required: true,
+        errorMessage: 'Поле обязательно к заполнению'
+    });
+
+    const lastNameReg = new Input({
+        wrapperSelector: `.re-last-name-reg`,
+        required: true,
+        errorMessage: 'Поле обязательно к заполнению'
+    });
+
+    const emailReg = new Input({
+        wrapperSelector: `.re-email-reg`,
+        required: true,
+        validMask: /^([a-z0-9_\-\.]+)@([a-z0-9_\-\.]+)$/,
+        errorMessage: 'Введите email в корректном формате'
+    });
+
+    const passReg = new Input({
+        wrapperSelector: `.re-pass-reg`,
+        required: true,
+        errorMessage: 'Поле обязательно к заполнению'
+    }); 
+
+    const confPassReg = new Input({
+        wrapperSelector: `.re-conf-pass-reg`,
+        required: true,
+        errorMessage: 'Поле обязательно к заполнению'
+    }); 
+
+    const regReqFields = [
+        phoneReg,
+        nameReg,
+        lastNameReg,
+        emailReg,
+        passReg,
+        confPassReg,
+    ];
+
+    $('.reg-submit-form').on('click', (e) => {
+
+        regReqFields.forEach(field => {
+            if (!field.isValidValue()) {
+                field.setError()
+                e.preventDefault();
+            }
+        });
+
+    })
+
     if ($('#phone').length != 0) {
         $('#phone').inputmask({"mask": "+7 (999) 999-99-99"});
         $('#phone').val('')
