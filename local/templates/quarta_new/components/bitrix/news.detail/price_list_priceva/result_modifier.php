3174ea39c48655a0adb92da9728d07be2abeaa87
Alexander <shcheglakov@addamant.ru>
2024-07-04 12:46:40 +0600

    111888 | Сайт / Создание страницы прайса

---
+++ local/templates/quarta_new/components/bitrix/news.detail/price_list_priceva/result_modifier.php
@@ -0,0 +1,45 @@
+<?php
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+use Bitrix\Main\IO,
+    Helpers\FileSizeHelper,
+    Bitrix\Main\Application;
+
+$documentRoot = Application::getDocumentRoot();
+
+$dirPriceList = new IO\Directory(
+    implode('/', [
+        $documentRoot,
+        'upload',
+        'price_list_priceva'
+    ])
+);
+
+if ($dirPriceList->isExists()) {
+
+    $arFiles = $dirPriceList->getChildren();
+
+    if (!empty($arFiles)) {
+        foreach ($arFiles as $arFileIndex => $arFile) {
+
+            $pathFile = $arFile->getPath();
+
+            $obFileData = new IO\File(
+                $pathFile
+            );
+
+            $arResult['FILES'][$arFileIndex]['NAME'] = $obFileData->getName();
+            $arResult['FILES'][$arFileIndex]['SIZE'] = FileSizeHelper::getFormattedSize($obFileData->getSize());
+            $arResult['FILES'][$arFileIndex]['SRC'] = str_replace(
+                $documentRoot,
+                '',
+                $pathFile
+            );
+
+        }
+    }
+
+}
