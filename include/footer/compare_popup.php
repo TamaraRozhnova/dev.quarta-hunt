10921a38c44d824e3535031ee83dc4adf9efb1e8
Alexander <shcheglakov@addamant.ru>
2024-08-06 14:57:44 +0600

    113088 | Сайт / Создание попап окна Сравнение

---
+++ include/footer/compare_popup.php
@@ -0,0 +1,5 @@
+<div class="compare-popup">
+    <div class="compare-popup__text">Вы добавили товары к сравнению, нажмите чтобы перейти в сравнение</div>
+    <a class="btn btn-primary" href='/compare/'>Перейти</a>
+    <div class="compare-popup__clear btn"> Очистить</div>
+</div>
