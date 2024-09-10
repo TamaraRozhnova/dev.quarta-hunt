6d1099d419f0ee68e891d4de86b56d4fbfc4b034
aminakin <minakin@addamant.ru>
2024-09-10 12:03:43 +0200

    113931 | правки

--- local/ajax/hut/subscribeHandler.php
+++ local/ajax/hut/subscribeHandler.php
@@ -4,11 +4,13 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.
 use Bitrix\Main\Context;
 use Form\MailSubscribe;
 
+$APPLICATION->RestartBuffer();
+
 $request = Context::getCurrent()->getRequest();
 $email = $request->get("sf_EMAIL");
 
 if (!empty($email)) {
     echo MailSubscribe::subcribeEmailHandler($email);
 } else {
-    echo 'nothing';
+    echo json_encode(['status' => 'nothing']);
 }
