dfe5ffc8c0714a47f6d0e15cdd2ffb87639d4e4c
aminakin <minakin@addamant.ru>
2024-09-12 18:43:16 +0200

    113932 | catalog.item

---
+++ local/templates/hut/components/bitrix/catalog.item/item/result_modifier.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+if (!empty($arResult['ITEM']['LABEL'])) {
+    $arResult['ITEM']['LABEL_IMG'] = $arResult['ITEM']['PROPERTIES'][key($arResult['ITEM']['LABEL_ARRAY_VALUE'])]['VALUE_XML_ID'];
+}
