03161314dd5a8871512214a9645efc3c3120eb3e
megamir2 <89516884+megamir2@users.noreply.github.com>
2024-10-01 09:51:24 +0600

    Add files via upload

---
+++ local/templates/hut/components/bitrix/news.list/faq/template.php
@@ -0,0 +1,47 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+
+
+
+<div class="faq__quests">
+	<?foreach($arResult["ITEMS"] as $i => $arItem):?>
+	<?
+	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+	?>
+	<div class="faq__quest <?=$i == 0 ? 'active' : ''?>"  id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+		<button class="faq__questBtn">
+			<span><?=$arItem['NAME']?></span>
+			<div class="faq__questBtnSvg">
+				<svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+					xmlns="http://www.w3.org/2000/svg">
+					<path fill-rule="evenodd" clip-rule="evenodd"
+						d="M12 4C12.5523 4 13 4.44772 13 5V11H19C19.5523 11 20 11.4477 20 12C20 12.5523 19.5523 13 19 13H13V19C13 19.5523 12.5523 20 12 20C11.4477 20 11 19.5523 11 19V13H5C4.44772 13 4 12.5523 4 12C4 11.4477 4.44772 11 5 11H11V5C11 4.44772 11.4477 4 12 4Z"
+						fill="#354052" />
+				</svg>
+				<svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+					xmlns="http://www.w3.org/2000/svg">
+					<path fill-rule="evenodd" clip-rule="evenodd"
+						d="M4 12C4 11.4477 4.44772 11 5 11H19C19.5523 11 20 11.4477 20 12C20 12.5523 19.5523 13 19 13H5C4.44772 13 4 12.5523 4 12Z"
+						fill="#354052" />
+				</svg>
+			</div>
+		</button>
+		<div class="faq__questCont">
+			<?=$arItem['PREVIEW_TEXT']?>
+		</div>
+	</div>
+	<?endforeach;?>
+</div>
