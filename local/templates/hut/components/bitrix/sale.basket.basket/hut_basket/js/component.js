d52eac598eebfa05a42e7817c2ac15199460bbaf
root <denisov@addamant.ru>
2024-10-30 11:09:58 +0300

    правки корзины

--- local/templates/hut/components/bitrix/sale.basket.basket/hut_basket/js/component.js
+++ local/templates/hut/components/bitrix/sale.basket.basket/hut_basket/js/component.js
@@ -2420,24 +2420,3 @@
 		}
 	};
 })();
-
-document.addEventListener('DOMContentLoaded', function() {
-	let basketItems = document.querySelectorAll('.basket-item');
-	if (basketItems) {
-		basketItems.forEach(item => {
-			let openRemoveItem = item.querySelector('.basket-item-actions-remove');
-			let removeModal = item.querySelector('.remove-item-block');
-			let closeModal = item.querySelector('.close-remove-item');
-
-			if (openRemoveItem && removeModal && closeModal) {
-				openRemoveItem.addEventListener('click', () => {
-					item.classList.add('remove');
-				});
-
-				closeModal.addEventListener('click', () => {
-					item.classList.remove('remove');
-				});
-			}
-		});
-	}
-});
