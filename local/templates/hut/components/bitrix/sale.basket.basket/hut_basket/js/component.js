fc2859e21affa419ce9ab7fa1f406c8ee677245b
root <denisov@addamant.ru>
2024-10-30 12:20:27 +0300

    hut changes | правка удаления товара из корзины

--- local/templates/hut/components/bitrix/sale.basket.basket/hut_basket/js/component.js
+++ local/templates/hut/components/bitrix/sale.basket.basket/hut_basket/js/component.js
@@ -1628,6 +1628,25 @@
 				{
 					this.filter.highlightSearchMatch(this.items[itemId]);
 				}
+
+				let basketItems = document.querySelectorAll('.basket-item');
+				if (basketItems) {
+					basketItems.forEach(item => {
+						let openRemoveItem = item.querySelector('.basket-item-actions-remove');
+						let removeModal = item.querySelector('.remove-item-block');
+						let closeModal = item.querySelector('.close-remove-item');
+
+						if (openRemoveItem && removeModal && closeModal) {
+							openRemoveItem.addEventListener('click', () => {
+								item.classList.add('remove');
+							});
+
+							closeModal.addEventListener('click', () => {
+								item.classList.remove('remove');
+							});
+						}
+					});
+				}
 			}
 		},
 
@@ -2420,3 +2439,24 @@
 		}
 	};
 })();
+
+document.addEventListener('DOMContentLoaded', function() {
+	let basketItems = document.querySelectorAll('.basket-item');
+	if (basketItems) {
+		basketItems.forEach(item => {
+			let openRemoveItem = item.querySelector('.basket-item-actions-remove');
+			let removeModal = item.querySelector('.remove-item-block');
+			let closeModal = item.querySelector('.close-remove-item');
+
+			if (openRemoveItem && removeModal && closeModal) {
+				openRemoveItem.addEventListener('click', () => {
+					item.classList.add('remove');
+				});
+
+				closeModal.addEventListener('click', () => {
+					item.classList.remove('remove');
+				});
+			}
+		});
+	}
+});
