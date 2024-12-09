34d65bdf8780b6c7952c480bd305bbafbf55c172
mebiketov <mebiketov@1cbit.ru>
2024-12-09 16:25:24 +0700

    feat: миграция пользовательского поля разделов каталогов UF_PROPS_ANALOG

---
+++ local/php_interface/migrations/Version20241209121915.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version20241209121915 extends Version
+{
+    protected $description = "Поле для разделов каталога UF_PROPS_ANALOG";
+
+    protected $moduleVersion = "4.2.4";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+        $helper->UserTypeEntity()->saveUserTypeEntity(array (
+  'ENTITY_ID' => 'IBLOCK_1c_catalog:catalog1c_main_SECTION',
+  'FIELD_NAME' => 'UF_PROPS_ANALOG',
+  'USER_TYPE_ID' => 'iblock_property',
+  'XML_ID' => 'UF_PROPS_ANALOG',
+  'SORT' => '100',
+  'MULTIPLE' => 'Y',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'DISPLAY' => 'CHECKBOX',
+    'LIST_HEIGHT' => 1,
+    'IBLOCK_ID' => 16,
+    'DEFAULT_VALUE' => '',
+    'ACTIVE_FILTER' => 'Y',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
