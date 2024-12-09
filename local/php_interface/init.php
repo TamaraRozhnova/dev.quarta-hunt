86bb5b502f1a4e77dc1509ae6a9d157090fa7ec4
eo-33 <132452430+eo-33@users.noreply.github.com>
2024-12-09 11:52:45 +0300

    Revert "feat: блок аналогичных товаров на детальной странице товара"

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -2,7 +2,6 @@
 
 use \Bitrix\Main\Loader;
 use \Bitrix\Main\EventManager;
-use UserType\IblockPropertyType;
 
 if (!function_exists('chmod')) {
 	function chmod(...$args)
@@ -17,8 +16,6 @@ Loader::registerAutoLoadClasses(null, [
 	'CustomEvents\OnBeforeIBlockElementUpdate' => '/local/php_interface/classes/Events/OnBeforeIBlockElementUpdate.php',
 	'BitSaleExport' => '/local/php_interface/override_classes/BitSaleExport.php',
 	'CustomEvents\Hut\OnBeforeIBlockElementUpdate' => '/local/php_interface/classes/Events/Hut/OnBeforeIBlockElementUpdate.php',
-    'UserType\IblockPropertyType' => '/local/php_interface/classes/UserType/IblockPropertyType.php',
-    'UserType\IBlockElementEnum' => '/local/php_interface/classes/UserType/IBlockElementEnum.php',
 ]);
 
 $eventManager = EventManager::getInstance();
@@ -28,5 +25,3 @@ $eventManager->addEventHandler(
 	"OnBeforeIBlockElementUpdate",
 	["CustomEvents\OnBeforeIBlockElementUpdate", "OnBeforeIBlockElementUpdateHandler"]
 );
-
-$eventManager->addEventHandler('main', 'OnUserTypeBuildList', [IblockPropertyType::class, 'GetUserTypeDescription']);
