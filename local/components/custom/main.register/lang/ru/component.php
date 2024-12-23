84c2128a21a56d380a2f423cf4d191d5381059aa
Overspark <djonik1612@gmail.com>
2024-12-23 11:12:22 +0600

    118416 | проверка номера на уникальность при регистрации

--- local/components/custom/main.register/lang/ru/component.php
+++ local/components/custom/main.register/lang/ru/component.php
@@ -1,9 +1,11 @@
-<?
-$MESS ['REGISTER_WRONG_CAPTCHA'] = "Неверно введено слово с картинки";
-$MESS ['REGISTER_FIELD_REQUIRED'] = "Поле #FIELD_NAME# обязательно для заполнения";
-$MESS ['REGISTER_DEFAULT_TITLE'] = "Регистрация нового пользователя";
-$MESS ['REGISTER_USER_WITH_EMAIL_EXIST'] = "Пользователь с таким e-mail (#EMAIL#) уже существует.";
-$MESS["main_register_sess_expired"]="Ваша сессия истекла, повторите попытку регистрации.";
-$MESS["main_register_decode_err"]="Ошибка при дешифровании пароля (#ERRCODE#).";
-$MESS["main_register_error_sms"] = "Неверный код подтверждения из СМС.";
-?>
+<?php
+
+$MESS['REGISTER_WRONG_CAPTCHA'] = 'Неверно введено слово с картинки';
+$MESS['REGISTER_FIELD_REQUIRED'] = 'Поле #FIELD_NAME# обязательно для заполнения';
+$MESS['REGISTER_DEFAULT_TITLE'] = 'Регистрация нового пользователя';
+$MESS['REGISTER_USER_WITH_EMAIL_EXIST'] = 'Пользователь с таким e-mail (#EMAIL#) уже существует.';
+$MESS['main_register_sess_expired']='Ваша сессия истекла, повторите попытку регистрации.';
+$MESS['main_register_decode_err']='Ошибка при дешифровании пароля (#ERRCODE#).';
+$MESS['main_register_error_sms'] = 'Неверный код подтверждения из СМС.';
+$MESS['USER_WITH_THIS_PHONE_EXIST'] = 'Этот номер телефона уже используется в другом аккаунте.
+Пожалуйста, войдите в существующий аккаунт или укажите другой номер.';
