017778fabf398e1d2490d2763fec972af49bcba1
Di <x-di@bk.ru>
2024-12-27 16:49:51 +0700

    feat: Верстка меню на stalker.ru и shottime.ru

--- local/templates/stalker/components/bitrix/menu/smart_catalog_top/template.php
+++ local/templates/stalker/components/bitrix/menu/smart_catalog_top/template.php
@@ -33,12 +33,14 @@ $arSelect = [
 	'ID',
 	'LEFT_MARGIN',
 	'DEPTH_LEVEL',
+	'IBLOCK_SECTION_ID',
 	'NAME',
 	'PICTURE',
+	'UF_SVG',
 	'SECTION_PAGE_URL'
 ];
 
-$resSections = \CIBlockSection::GetList( $arOrder, $arFilter, false, $arSelect );
+$resSections = \CIBlockSection::GetList( $arOrder, $arFilter, true, $arSelect );
 
 while ($arSection = $resSections->GetNext())
 {
@@ -52,28 +54,70 @@ while ($arSection = $resSections->GetNext())
 	{
 		$list_1[ $i ]['UNDER'][ $arSection['ID'] ] = $arSection;
 	}
+	elseif ($arSection['DEPTH_LEVEL'] == 3)
+	{
+		$list_1[ $arSection['IBLOCK_SECTION_ID'] ]['UNDER'][ $arSection['ID'] ] = $arSection;
+
+	}
 }
 ?>
-	<? foreach ($list_1 as $main) { ?>
-		<div class="nav-catalog__item">
-			<div class="nav-catalog__item-img">
-				<picture>
-					<img class="lazy" data-src="<?=CFile::GetPath($main['PICTURE'])?>" alt=""/>
-				</picture>
-				<div class="dot dot--top-left"></div>
-				<div class="dot dot--top-right"></div>
-				<div class="dot dot--bottom-left"></div>
-				<div class="dot dot--bottom-right"></div>
-			</div>
-			<div class="nav-catalog__item-inner">
-				<a href="<?=$main['SECTION_PAGE_URL']?>" class="nav-catalog__item-title"><?=$main['NAME']?></a>
-				<ul>
-					<? foreach ($main['UNDER'] as $key => $under) { ?>
-						<li>
-							<a href="<?=$under['SECTION_PAGE_URL']?>" class="ui-link ui-link--underline"><?=$under['NAME']?></a>
-						</li>
+
+<div class="nav-catalog-cont2">
+	<div class="left">
+		<? foreach($list_1 AS $arItem) {  if($arItem['DEPTH_LEVEL'] != 1) continue;?>
+			<div class="nav-catalog-cont2__tab <? if(empty($k)) echo 'active';?>" data-id="<?=$arItem['ID']?>">
+
+				<span class="ico">
+					<? if(!empty($arItem['UF_SVG'])) { ?>
+						<img src="<?=CFile::GetPath($arItem['UF_SVG'])?>" alt="<?=$arItem['NAME']?>">
 					<? } ?>
-				</ul>
+				</span>
+				<?=$arItem['NAME']?>
 			</div>
-		</div>
-	<? } ?>
+		<? $k++; ?>
+		<? } ?>
+	</div>
+
+    <? $k = ''; ?>
+	<div class="right">
+        <div class="right_cont">
+            <? foreach($list_1 AS $arItem) { if($arItem['DEPTH_LEVEL'] != 1) continue; ?>
+            <div class="nav-catalog-cont2__tab_cont tab_cont_<?=$arItem['ID']?> <? if(empty($k)) echo 'active';?>">
+
+                <div class="nav-catalog-cont2__tab-mob nav-catalog-cont2__tab <? if(empty($k)) echo 'active';?>" data-id="<?=$arItem['ID']?>">
+
+                    <span class="ico">
+                        <? if(!empty($arItem['UF_SVG'])) { ?>
+                            <img src="<?=CFile::GetPath($arItem['UF_SVG'])?>" alt="<?=$arItem['NAME']?>">
+                        <? } ?>
+                    </span>
+                    <?=$arItem['NAME']?>
+                </div>
+                <div class="nav-catalog-cont2__mob">
+                    <div class="title"><span><?=$arItem['NAME']?></span> <?=$arItem['ELEMENT_CNT']?> товаров</div>
+                    <div class="menu_block">
+                        <? foreach($arItem['UNDER'] as $v) { ?>
+                            <div class="menu_block__item">
+                                <a href="<?=$v['SECTION_PAGE_URL']?>"><?=$v['NAME']?></a>
+                                <? if(!empty($list_1[ $v['ID'] ]['UNDER'])) { ?>
+                                    <ul>
+                                        <? foreach($list_1[ $v['ID'] ]['UNDER'] AS $item3) { ?>
+                                            <li>
+                                                <a href="<?=$item3['SECTION_PAGE_URL']?>"><?=$item3['NAME']?></a>
+                                            </li>
+                                        <? } ?>
+                                    </ul>
+                                <? } ?>
+                            </div>
+                        <? } ?>
+                    </div>
+                </div>
+
+            </div>
+            <? $k++; ?>
+            <? } ?>
+
+        </div>
+	</div>
+
+</div>
